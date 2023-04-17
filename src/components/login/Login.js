78920045f879bd7cc4fe245b0838ddad0b8a29b4
Jaime Muñoz <jaime.munozq@udea.edu.co>
2023-04-17 16:48:40 -0500

    agregar el login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div> Login   </div>
-  )
+    <>
+<div class="center">
+      <div class="image">
+        <img src='D:\UDEA\Semestre 3\analisis1\fabrica-escuela\src\images\login.jpg' alt="Udea" />
+      </div>
+      <div class="main_wrapper">
+        <h1>Ingreso</h1>
+        <form method="post">
+          <div class="text_field">
+            <input type="text" required />
+            <label>Usuario</label>
+          </div>
+          <div class="text_field">
+            <input type="password" required />
+            <label>Cntraseña</label>
+          </div>
+          <div class="pass">Olvido su contraseña?</div>
+          <input type="submit" value="Login" />
+          <div class="signup_link">
+            No está regustrado?
+            <a href="login">Registrarse </a>
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+  );
 }
-
